33418b9c601bd2a7adcf99e63f7ce71b966a3a35
Arnaud <arnaud.rabhi@limayrac.fr>
2023-05-16 12:01:14 +0200

    Ajout du reporting Matiere (Durée d'enseignement par matiere, dates de chaque cours de chaque matiere

--- src/Controller/ReportingController.php
+++ src/Controller/ReportingController.php
@@ -6,6 +6,7 @@ use App\Helper\ReportingHelper;
 use App\Repository\CoursRepository;
 use App\Repository\FormationRepository;
 use App\Repository\IntervenantRepository;
+use App\Repository\MatiereRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,12 +23,22 @@ class ReportingController extends AbstractController
     }
 
     #[Route('/reporting/intervenant', name: 'reporting_intervenant')]
-    public function reportingIntervenant(Request $request, FormationRepository $formationRepository, CoursRepository $coursRepository, IntervenantRepository $intervenantRepository) {
+    public function reportingIntervenant(Request $request, IntervenantRepository $intervenantRepository) {
 
         return $this->render('reporting/reportingIntervenant.html.twig', [
             'title' => 'Details Formation',
             'statsIntervenant' => ReportingHelper::statsIntervenant($intervenantRepository),
             'heuresIntervenant' => ReportingHelper::heuresIntervenant($intervenantRepository)
+        ]);
+    }
+
+    #[Route('/reporting/matiere', name: 'reporting_matiere')]
+    public function reportingMatiere(Request $request, MatiereRepository $matiereRepository) {
+
+        return $this->render('reporting/reportingMatiere.html.twig', [
+            'title' => 'Details Formation',
+            'statsMatiere' => ReportingHelper::statsMatiere($matiereRepository),
+            'heuresMatiere' => ReportingHelper::heuresMatiere($matiereRepository)
         ]);
     }
 }
--- src/Helper/ReportingHelper.php
+++ src/Helper/ReportingHelper.php
@@ -3,7 +3,10 @@
 namespace App\Helper;
 
 use App\Entity\Intervenant;
+use App\Entity\Matiere;
 use App\Repository\IntervenantRepository;
+use App\Repository\MatiereRepository;
+use JetBrains\PhpStorm\ArrayShape;
 use JetBrains\PhpStorm\Pure;
 
 class ReportingHelper
@@ -57,7 +60,7 @@ class ReportingHelper
             $heuresParIntervenant = [];
 
             foreach ($cours as $cour) {
-                $heuresParIntervenant[] = $cour->getDateCours()->format('Y-m-d') . ' de ' . $cour->getHeureDebut()->format("H:i") . ' à ' . $cour->getHeureFin()->format("H:i")  ;
+                $heuresParIntervenant[] = $cour->getMatiere()->getIntitule() . ' - Le ' . $cour->getDateCours()->format('Y-m-d') . ' de ' . $cour->getHeureDebut()->format("H:i") . ' à ' . $cour->getHeureFin()->format("H:i")  ;
             }
             $heuresIntervenant[$intervenant->getFullName()] = $heuresParIntervenant;
         }
@@ -69,7 +72,7 @@ class ReportingHelper
      *
      * @return array
      */
-    #[Pure] public static function totalMinutesParIntervenant(Intervenant $intervenant): array
+    #[ArrayShape(['minutes' => "int|null", 'couleur' => "string"])] #[Pure] public static function totalMinutesParIntervenant(Intervenant $intervenant): array
     {
         // En Minutes
         $totalMinutes = 0;
@@ -81,6 +84,62 @@ class ReportingHelper
             'minutes' => $totalMinutes,
             'couleur' => "#000000"
         ];
+    }
+
+    public static function totalMinutesParMatiere(Matiere $matiere): ?int
+    {
+        $totalMinutes = 0;
+        $cours = $matiere->getCours();
+        foreach ($cours as $cour) {
+            $totalMinutes += $cour->getDureeMinutes();
+        }
+        return $totalMinutes;
+    }
+
+    public static function totalMinutesMatieres(MatiereRepository $matiereRepository): ?int
+    {
+        $totalMinutes = 0;
+        $matieres = $matiereRepository->findAll();
+
+        foreach ($matieres as $matiere) {
+            $cours = $matiere->getCours();
+            foreach ($cours as $cour) {
+                $totalMinutes += $cour->getDureeMinutes();
+            }
+        }
+        return $totalMinutes;
+    }
+
+    /**
+     * @param MatiereRepository $matiereRepository
+     *
+     * @return array
+     */
+    public static function heuresMatiere(MatiereRepository $matiereRepository): array
+    {
+        $heuresMatiere = [];
+        $matieres = $matiereRepository->findAll();
+
+        foreach ($matieres as $matiere) {
+            $cours = $matiere->getCours();
+            $heuresParMatiere = [];
+
+            foreach ($cours as $cour) {
+                $heuresParMatiere[] = $cour->getDateCours()->format('Y-m-d') . ' de ' . $cour->getHeureDebut()->format("H:i") . ' à ' . $cour->getHeureFin()->format("H:i")  ;
+            }
+            $heuresMatiere[$matiere->getIntitule()] = $heuresParMatiere;
+        }
+        return $heuresMatiere;
+    }
+
+    public static function statsMatiere(MatiereRepository $matiereRepository) {
+        $matieres = $matiereRepository->findAll();
+        $statsMatieres = ['Total : ' => self::totalMinutesMatieres($matiereRepository)];
+        foreach ($matieres as $matiere) {
+
+            $statsMatieres[$matiere->getIntitule()] = self::totalMinutesParMatiere($matiere);
+        }
+        return $statsMatieres;
     }
 
 }
